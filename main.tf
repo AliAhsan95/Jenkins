da3af6d857b3544277ef8f3426dd79708c7023ca
Cloud User <ec2-user@ip-172-31-20-173.ec2.internal>
2024-11-03 23:29:22 +0000

    new update

--- main.tf
+++ main.tf
@@ -2,26 +2,51 @@ provider "aws" {
   region = "us-east-1"  
 }
 
-resource "aws_instance" "ubuntu1_instance" {
-  ami           = "ami-0866a3c8686eaeeba" 
-  instance_type = "t2.micro"
+# Define a security group that allows HTTP and SSH access
+resource "aws_security_group" "allow_http_and_ssh" {
+  name        = "allow_http_and_ssh"
+  description = "Allow HTTP and SSH inbound traffic"
 
-   vpc_security_group_ids = [aws_security_group.allow_http_and_ssh.id]
-    key_name = "devopskeypair2"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Ubuntu EC2 instance
+resource "aws_instance" "ubuntu1_instance" {
+  ami                    = "ami-0866a3c8686eaeeba"  # Replace with the Ubuntu AMI ID
+  instance_type          = "t2.micro"
+  key_name               = "devopskeypair2"  # Replace with your EC2 key pair name
+  vpc_security_group_ids = [aws_security_group.allow_http_and_ssh.id]  # Reference the security group ID
 
   tags = {
     Name = "Ubuntu-Instance"
   }
 }
 
+# RHEL EC2 instance
 resource "aws_instance" "rhel1_instance" {
-  ami           = "ami-0583d8c7a9c35822c"  
-  instance_type = "t2.micro"
-
- vpc_security_group_ids = [aws_security_group.allow_http_and_ssh.id]
-    key_name = "devopskeypair2"
-
-
+  ami                    = "ami-0583d8c7a9c35822c"  # Replace with the RHEL AMI ID
+  instance_type          = "t2.micro"
+  key_name               = "devopskeypair2"  # Replace with your EC2 key pair name
+  vpc_security_group_ids = [aws_security_group.allow_http_and_ssh.id]  # Reference the security group ID
 
   tags = {
     Name = "RHEL-Instance"
