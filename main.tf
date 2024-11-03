38b93d472db5df460a66b8b2437414e2f297dcd7
Cloud User <ec2-user@ip-172-31-20-173.ec2.internal>
2024-11-03 23:21:34 +0000

    new changes

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"  
 }
 
-resource "aws_instance" "ubuntu_instance" {
+resource "aws_instance" "ubuntu1_instance" {
   ami           = "ami-0866a3c8686eaeeba" 
   instance_type = "t2.micro"
 
@@ -14,7 +14,7 @@ resource "aws_instance" "ubuntu_instance" {
   }
 }
 
-resource "aws_instance" "rhel_instance" {
+resource "aws_instance" "rhel1_instance" {
   ami           = "ami-0583d8c7a9c35822c"  
   instance_type = "t2.micro"
 
