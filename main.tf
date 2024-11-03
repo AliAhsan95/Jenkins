aed635bc1a078b88d9ba5ae4b4682e12d4d59aa4
Cloud User <ec2-user@ip-172-31-20-173.ec2.internal>
2024-11-03 22:52:02 +0000

    Ubuntu&Rhel

--- main.tf
+++ main.tf
@@ -1,15 +1,30 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"  
 }
 
-resource "aws_instance" "amazon_linx" {
-    count = 1
-    ami = "ami-06b21ccaeff8cd686"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.allow_http_and_ssh.id]
+resource "aws_instance" "ubuntu_instance" {
+  ami           = "ami-0866a3c8686eaeeba" 
+  instance_type = "t2.micro"
+
+   vpc_security_group_ids = [aws_security_group.allow_http_and_ssh.id]
     key_name = "devopskeypair2"
-    tags = {
-        name = "aws_instance"
-    }
+
+  tags = {
+    Name = "Ubuntu-Instance"
+  }
+}
+
+resource "aws_instance" "rhel_instance" {
+  ami           = "ami-0583d8c7a9c35822c"  
+  instance_type = "t2.micro"
+
+ vpc_security_group_ids = [aws_security_group.allow_http_and_ssh.id]
+    key_name = "devopskeypair2"
+
+
+
+  tags = {
+    Name = "RHEL-Instance"
+  }
 }
 
